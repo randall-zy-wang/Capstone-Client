f38ebc9544203287b9b3374d517c65099a39eb5e
wangz282-1838578 <wangz282@uw.edu>
2022-03-03 16:59:26 -0800

    jerry posts page

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -3,6 +3,7 @@ import Copyright from "./Copyright";
 import PostCard from "./PostCard";
 import closeIcon from "../photos/icons8-close-16.png";
 import plusIcon from "../photos/icons8-plus-64.png";
+import { useEffect } from "react";
 
 const MAX_ITEMS_PER_PAGE = 5;
 
@@ -16,70 +17,43 @@ function closeCreatePostModal() {
 
 const Posts = () => {
   const [page, setPage] = useState(1);
-  const [postCardData, setPostCardData] = useState(async() => { 
+  const [postCardData, setPostCardData] = useState({});
+
+  useEffect(() => {
     fetch(`/api/posts`)
       .then((response) => response.json())
-        .then((data) => console.log(data))
-  });
-  postCardData.then(data => console.log(data))
-  //   [{
-  //     title: "Maddy",
-  //     type: "Cat",
-  //     dates: "Feb 10 - Feb 12",
-  //     description:
-  //       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillumdolore eu fugiat nulla pariatur.",
-  //     image: "https://http.cat/202",
-  //   },
-  //   {
-  //     title: "Fishcake",
-  //     type: "Dog",
-  //     dates: "Feb 20 - Feb 22",
-  //     description:
-  //       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  //     image: "",
-  //   },
-  //   {
-  //     title: "Hawk",
-  //     type: "Fish",
-  //     dates: "Feb 10 - Feb 12",
-  //     description:
-  //       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  //     image: "",
-  //   },
-  //   {
-  //     title: "Bond",
-  //     type: "Rabbit",
-  //     dates: "Feb 10 - Feb 12",
-  //     description:
-  //       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  //     image: "",
-  //   },
-  // ]
+        .then(function(data) {
+          setPostCardData(data)
+        })
+  }, [page])
   const postCardElements = [];
   const start = Math.min((page - 1) * MAX_ITEMS_PER_PAGE, postCardData.length);
   const end = Math.min(page * MAX_ITEMS_PER_PAGE, postCardData.length);
   for (let i = start; i < end; i++) {
-    const newIndex = postCardElements.length;
+    // const newIndex = postCardElements.length;
+    // let cleanStart = postCardData[i].start_date.getDateWithoutTime()
+    // let cleanEnd = postCardData[i].end_date.getDateWithoutTime()
     postCardElements.push(
       <PostCard
-        petName={postCardData[i].title}
-        petType={postCardData[i].type}
+        key={postCardData[i].pet_name}
+        pet_name={postCardData[i].pet_name}
+        pet_type={postCardData[i].pet_type}
         start_date={postCardData[i].start_date}
         end_date={postCardData[i].end_date}
         description={postCardData[i].description}
+        img={postCardData[i].img}
         // image={postCardData[i].image}
-        onDelete={() => {
-          if (
-            window.confirm("Are you really really sure that you wanna delete?")
-          ) {
-            const newArray = postCardData.slice();
-            newArray.splice(newIndex, 1);
-            setPostCardData(newArray);
-          }
-        }}
+        // onDelete={() => {
+        //   if (
+        //     window.confirm("Are you really really sure that you wanna delete?")
+        //   ) {
+        //     const newArray = postCardData.slice();
+        //     newArray.splice(newIndex, 1);
+        //     setPostCardData(newArray);
+        //   }
+        // }}
       />
     );
-    console.log(postCardElements)
   }
   return (
     <main id="posts">
@@ -113,27 +87,32 @@ const Posts = () => {
             <div className="modal-body">
               <div class="form__input-group">
                 <label for="petName">Pet Name</label>
-                <input type="text" class="form__input" id="petName"></input>
+                <input type="text" class="form__input" id="pet_name"></input>
                 <div class="form__input-error-message"></div>
               </div>
               <div class="form__input-group">
                 <label for="petType">Pet Type</label>
-                <input type="text" class="form__input" id="petType"></input>
+                <input type="text" class="form__input" id="pet_type"></input>
                 <div class="form__input-error-message"></div>
               </div>
               <div class="form__input-group">
                 <label for="startDate">Start date</label>
-                <input type="date" class="form__input" id="startDate"></input>
+                <input type="date" class="form__input" id="start_date"></input>
                 <div class="form__input-error-message"></div>
               </div>
               <div class="form__input-group">
                 <label for="endDate">End date</label>
-                <input type="date" class="form__input" id="endDate"></input>
+                <input type="date" class="form__input" id="end_date"></input>
                 <div class="form__input-error-message"></div>
               </div>
-              <div class="form__input-group">
+              {/* <div class="form__input-group">
                 <label for="petPhoto">Pet Photo</label>
                 <input type="file" class="form__input" id="petPhoto"></input>
+                <div class="form__input-error-message"></div>
+              </div> */}
+              <div class="form__input-group">
+                <label for="img_link">Image Link</label>
+                <input type="text" class="form__input" id="img_link"></input>
                 <div class="form__input-error-message"></div>
               </div>
               <div class="form__input-group">
@@ -190,37 +169,39 @@ const Posts = () => {
 async function storePosts() {
   try{
     // document.getElementById("postStatus").innerHTML = "sending data..."
-    let petName = document.getElementById("petName").value;
-    let petType = document.getElementById("petType").value;
-    let start_date = document.getElementById("startDate").value;
-    let end_date = document.getElementById("endDate").value;
+    let pet_name = document.getElementById("pet_name").value;
+    let pet_type = document.getElementById("pet_type").value;
+    let start_date = document.getElementById("start_date").value;
+    let end_date = document.getElementById("end_date").value;
     let description = document.getElementById("description").value;
+    let img = document.getElementById("img_link").value;
     // store the image
 
     const myData = {
-      petName: petName,
-      petType: petType,
+      pet_name: pet_name,
+      pet_type: pet_type,
       description: description,
       start_date: start_date,
-      end_date: end_date
-      // store thr image here
+      end_date: end_date,
+      img: img
     };
 
-    console.log(myData)
-    let postPetResponse = await fetch(`/users/signup`,
+    // console.log(myData)
+    let postPetResponse = await fetch(`/api/posts`,
         {method: "POST", body: JSON.stringify(myData), headers: {'Content-Type': 'application/json'}}
     )
     let status = await postPetResponse.json();
     if(status.status === "error"){
-        alert("Error:" + status.error);
+        alert("Error: ", status.error);
     } else {
-        document.getElementById("petName").value = "";
-        document.getElementById("petType").value = "";
+        document.getElementById("pet_name").value = "";
+        document.getElementById("pet_type").value = "";
         document.getElementById("description").innerHTML = "";
-        document.getElementById("startDate").innerHTML = "";
-        document.getElementById("endDate").innerHTML = "";
+        document.getElementById("start_date").innerHTML = "";
+        document.getElementById("end_date").innerHTML = "";
+        document.getElementById("img_link").innerHTML = "";
         alert("successfully uploaded")
-        // loadPosts();
+        window.location.href = "/api/posts"
       }
       closeCreatePostModal();
   }catch(error){
