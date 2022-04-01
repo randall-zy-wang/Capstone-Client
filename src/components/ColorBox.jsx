25fce36874e3f8f1f7f3f436e2051ed747bb861d
hongyt <hongyt@uw.edu>
2022-04-01 13:50:39 -0700

    merge conflict

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-export default function({color}) {
-    const style = {
-        backgroundColor: color
-    }
+export default function ({ color }) {
+  const style = {
+    backgroundColor: color,
+  };
 
-    return <div
-        className='color-box'
-        style={style}
-    />
-=======
-export default function({color}) {
-    const style = {
-        backgroundColor: color
-    }
-
-    return <div
-        className='color-box'
-        style={style}
-    />
->>>>>>> 765688037f6af93ba67a4313582459628b141d3e
+  return <div className="color-box" style={style} />;
 }
