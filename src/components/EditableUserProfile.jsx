a45c5ba0ecf302c1ee0503fbc7cb8d79e72e1817
hfl1212 <hongfei.lin12@gmail.com>
2022-05-03 17:44:15 -0700

    Adding profile image upload button and functionality

--- src/components/EditableUserProfile.jsx
+++ src/components/EditableUserProfile.jsx
@@ -56,221 +56,225 @@ export default function EditableUserProfile({ stored, editCompleteCallback }) {
   }
 
   return (
-    <div className="profile-edit">
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">My Name:</div>
-        <input
-          type="text"
-          id="username"
-          value={userinfo.username}
-          onChange={(e) => {
-            userinfo.username = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
-
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Profile photo</div>
-        <input type="file" onChange={(e) => uploadPhoto(e)} />
-        <img
-          alt=""
-          style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
-          src={userinfo.headimg || wuyanzu}
-        ></img>
-      </div>
-
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">
-          Phone number
+    <form action="/profile/upload" method="POST" enctype="multipart/form-data">
+      <div className="profile-edit">
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">My Name:</div>
+          <input
+            type="text"
+            id="username"
+            value={userinfo.username}
+            onChange={(e) => {
+              userinfo.username = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
         </div>
-        <input
-          type="text"
-          value={userinfo.contact.phone}
-          onChange={(e) => {
-            userinfo.contact.phone = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">
+
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Profile photo</div>
+          <input type="file" name="file" id="file" onChange={(e) => uploadPhoto(e)} />
+          {/* <input type="submit" value="Submit" class="btn btn-primary" /> */}
+          <button className="form__button" type="submit">Upload</button>
           <img
             alt=""
-            style={{ width: 30, height: 30, marginRight: 10, borderRadius: 50 }}
-            src={facebook}
+            style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
+            src={userinfo.headimg || wuyanzu}
           ></img>
-          Facebook
         </div>
-        <input
-          type="text"
-           value={userinfo.contact.facebook}
-          onChange={(e) => {
-            userinfo.contact.facebook = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
 
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">
-          <img
-            alt=""
-            style={{ width: 30, height: 30, marginRight: 10, borderRadius: 50 }}
-            src={instagram}
-          ></img>
-          Instagram
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">
+            Phone number
+          </div>
+          <input
+            type="text"
+            value={userinfo.contact.phone}
+            onChange={(e) => {
+              userinfo.contact.phone = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
         </div>
-        <input
-          type="text"
-           value={userinfo.contact.instagram}
-          onChange={(e) => {
-            userinfo.contact.instagram = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
-      <div
-        style={{
-          fontWeight: "bold",
-          fontSize: 18,
-          color: "#000",
-          textAlign: "left",
-          margin: 10,
-        }}
-      >
-        Pet info
-      </div>
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet Name:</div>
-        <input
-          type="text"
-          value={userinfo.pets[0].name}
-          onChange={(e) => {
-            userinfo.pets[0].name = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet Type:</div>
-        <input
-          type="text"
-          value={userinfo.pets[0].type}
-          onChange={(e) => {
-            userinfo.pets[0].type = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet Breed:</div>
-        <input
-          type="text"
-          value={userinfo.pets[0].breed}
-          onChange={(e) => {
-            userinfo.pets[0].breed = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet Size:</div>
-        <input
-          type="int"
-          value={userinfo.pets[0].size}
-          onChange={(e) => {
-            userinfo.pets[0].size = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet Gender:</div>
-        <input
-          type="text"
-          value={userinfo.pets[0].gender}
-          onChange={(e) => {
-            userinfo.pets[0].gender = e.target.value;
-            setUserinfo({ ...userinfo })
-          }}
-        />
-      </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">
+            <img
+              alt=""
+              style={{ width: 30, height: 30, marginRight: 10, borderRadius: 50 }}
+              src={facebook}
+            ></img>
+            Facebook
+          </div>
+          <input
+            type="text"
+            value={userinfo.contact.facebook}
+            onChange={(e) => {
+              userinfo.contact.facebook = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
+        </div>
 
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet Age:</div>
-        <input
-          type="text"
-          value={userinfo.pets[0].age}
-          onChange={(e) => {
-            userinfo.pets[0].age = e.target.value;
-            setUserinfo({ ...userinfo })
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">
+            <img
+              alt=""
+              style={{ width: 30, height: 30, marginRight: 10, borderRadius: 50 }}
+              src={instagram}
+            ></img>
+            Instagram
+          </div>
+          <input
+            type="text"
+            value={userinfo.contact.instagram}
+            onChange={(e) => {
+              userinfo.contact.instagram = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
+        </div>
+        <div
+          style={{
+            fontWeight: "bold",
+            fontSize: 18,
+            color: "#000",
+            textAlign: "left",
+            margin: 10,
           }}
-        />
-      </div>
+        >
+          Pet info
+        </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet Name:</div>
+          <input
+            type="text"
+            value={userinfo.pets[0].name}
+            onChange={(e) => {
+              userinfo.pets[0].name = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
+        </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet Type:</div>
+          <input
+            type="text"
+            value={userinfo.pets[0].type}
+            onChange={(e) => {
+              userinfo.pets[0].type = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
+        </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet Breed:</div>
+          <input
+            type="text"
+            value={userinfo.pets[0].breed}
+            onChange={(e) => {
+              userinfo.pets[0].breed = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
+        </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet Size:</div>
+          <input
+            type="int"
+            value={userinfo.pets[0].size}
+            onChange={(e) => {
+              userinfo.pets[0].size = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
+        </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet Gender:</div>
+          <input
+            type="text"
+            value={userinfo.pets[0].gender}
+            onChange={(e) => {
+              userinfo.pets[0].gender = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
+        </div>
 
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet photo 1</div>
-        <input type="file" onChange={(e) => uploadPet(e, 1)} />
-        {userinfo.pet1 ? (
-          <img
-            alt=""
-            style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
-            src={userinfo.pet1}
-          ></img>
-        ) : (
-          ""
-        )}
-      </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet Age:</div>
+          <input
+            type="text"
+            value={userinfo.pets[0].age}
+            onChange={(e) => {
+              userinfo.pets[0].age = e.target.value;
+              setUserinfo({ ...userinfo })
+            }}
+          />
+        </div>
 
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet photo 2</div>
-        <input type="file" onChange={(e) => uploadPet(e, 2)} />
-        {userinfo.pet2 ? (
-          <img
-            alt=""
-            style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
-            src={userinfo.pet2}
-          ></img>
-        ) : (
-          ""
-        )}
-      </div>
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet photo 3</div>
-        <input type="file" onChange={(e) => uploadPet(e, 3)} />
-        {userinfo.pet3 ? (
-          <img
-            alt=""
-            style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
-            src={userinfo.pet3}
-          ></img>
-        ) : (
-          ""
-        )}
-      </div>
-      <div className="profile-edit-row">
-        <div className="profile-edit-row-name">Pet photo 4</div>
-        <input type="file" onChange={(e) => uploadPet(e, 4)} />
-        {userinfo.pet4 ? (
-          <img
-            alt=""
-            style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
-            src={userinfo.pet4}
-          ></img>
-        ) : (
-          ""
-        )}
-      </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet photo 1</div>
+          <input type="file" onChange={(e) => uploadPet(e, 1)} />
+          {userinfo.pet1 ? (
+            <img
+              alt=""
+              style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
+              src={userinfo.pet1}
+            ></img>
+          ) : (
+            ""
+          )}
+        </div>
 
-      <div>
-        <button className="profile-edit-button" onClick={handleSaveClicked}>
-          Save
-        </button>
-        <button className="profile-edit-button" onClick={handleCancelClicked}>
-          Cancel
-        </button>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet photo 2</div>
+          <input type="file" onChange={(e) => uploadPet(e, 2)} />
+          {userinfo.pet2 ? (
+            <img
+              alt=""
+              style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
+              src={userinfo.pet2}
+            ></img>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet photo 3</div>
+          <input type="file" onChange={(e) => uploadPet(e, 3)} />
+          {userinfo.pet3 ? (
+            <img
+              alt=""
+              style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
+              src={userinfo.pet3}
+            ></img>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="profile-edit-row">
+          <div className="profile-edit-row-name">Pet photo 4</div>
+          <input type="file" onChange={(e) => uploadPet(e, 4)} />
+          {userinfo.pet4 ? (
+            <img
+              alt=""
+              style={{ width: 100, height: 100, margin: 10, borderRadius: 50 }}
+              src={userinfo.pet4}
+            ></img>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div>
+          <button className="profile-edit-button" onClick={handleSaveClicked}>
+            Save
+          </button>
+          <button className="profile-edit-button" onClick={handleCancelClicked}>
+            Cancel
+          </button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
